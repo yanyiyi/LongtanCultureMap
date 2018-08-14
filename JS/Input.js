f57b8abd4af167d9e3d6d5e185494dc61ed01787
DePoLK <ms0507030@gmail.com>
2018-08-14 18:43:39 +0800

    input.html innerhtml update

---
+++ JS/Input.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    
+    
+    $('#infocontent').click(function(){
+        ReSetInfoContent();
+        $('#infocontent').animate({
+            height:"200px"
+        },500);
+    });//end infocontent click
+    $('#infocontent2').click(function(){
+        ReSetInfoContent();
+        $('#infocontent2').animate({
+            height:"200px"
+        },500);
+    });//end infocontent2 click
+    $('#infocontent3').click(function(){
+        ReSetInfoContent();
+        $('#infocontent3').animate({
+            height:"200px"
+        },500);
+    });//end infocontent3 click
+    $('#infocontent4').click(function(){
+        ReSetInfoContent();
+        $('#infocontent4').animate({
+            height:"200px"
+        },500);
+    });//end infocontent4 click
+    $('#infocontent5').click(function(){
+        ReSetInfoContent();
+        $('#infocontent5').animate({
+            height:"200px"
+        },500);
+    });//end infocontent5 click
+    $('#infocontent6').click(function(){
+        ReSetInfoContent();
+        $('#infocontent6').animate({
+            height:"200px"
+        },500);
+    });//end infocontent6 click
+    $('#textdata').click(function(){
+        ReSetInfoContent();
+        $('#textdata').animate({
+            height:"200px"
+        },500);
+    });//end textdata click
+    
+    function ReSetInfoContent(){
+        $('#infocontent').animate({
+            height:"30px"
+        },400);
+        $('#infocontent2').animate({
+            height:"30px"
+        },400);
+        $('#infocontent3').animate({
+            height:"30px"
+        },400);
+        $('#infocontent4').animate({
+            height:"30px"
+        },400);
+        $('#infocontent5').animate({
+            height:"30px"
+        },400);
+        $('#infocontent6').animate({
+            height:"30px"
+        },400);
+        $('#textdata').animate({
+            height:"30px"
+        },400);
+    }
+    
+    
+});// end ready
+
